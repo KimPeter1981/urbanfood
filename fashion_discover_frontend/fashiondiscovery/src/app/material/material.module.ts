e16e0af382cfd5db0b31d0d1f1040db0439d7366
Kim Peter Mesenhoeller <kpm@netempire.de>
2020-11-21 22:08:12 +0100

    Implementation of Fashion Discover Header

---
+++ fashion_discover_frontend/fashiondiscovery/src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon'
+import {MatBadgeModule} from '@angular/material/badge'
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {MatToolbarModule} from '@angular/material/toolbar'
+
+const MaterialComponents = [MatButtonModule, MatButtonToggleModule, MatIconModule,
+                            MatBadgeModule, MatProgressSpinnerModule, MatToolbarModule];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
